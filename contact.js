a8ccf0c8ed8aed7817ced72d6b41a700e28562eb
Shekhar <shekhar59324@gmail.com>
2020-06-24 16:32:50 +0530

    Updated contact form

--- contact.js
+++ contact.js
@@ -7,15 +7,17 @@ function sendMail(){
     var state = document.getElementById("validationCustom04").value;
     var pincode = document.getElementById("validationCustom05").value;
     var userEmail = document.getElementById("exampleInputEmail1").value;
+    var userMsg = document.getElementById("exampleInputMsg").value;
+    
     Email.send({
 
         Host : 'smtp.gmail.com',
         Username : "mailforwardingexamcentre@gmail.com",
         Password : "ArduinoBoard",
-        To : 'shekharbrilliant@gmail.com',
+        To : 'exam.centre2020@gmail.com',
         From : `${userEmail}`,
         Subject : "Contact Form Details (Exam-Centre)",
-        Body : `<br><p>First Name : ${firstName}</p><p>Last Name : ${lastName}</p><p>City  : ${city}</p><p>State : ${state}</p><p>Pin Code : ${pincode}</p><p>Email : ${userEmail}</p>`
+        Body : `<br><p>First Name : ${firstName}</p><p>Last Name : ${lastName}</p><p>City  : ${city}</p><p>State : ${state}</p><p>Pin Code : ${pincode}</p><p>Email : ${userEmail}</p><p>Message : ${userMsg}</p>`
     }).then(
         message => alert("Mail Send")
     );
