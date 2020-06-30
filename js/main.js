4e47f70384fea424c3fbbd40c1d9bc6ad5ca7e4b
Shekhar <shekhar59324@gmail.com>
2020-06-29 21:52:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,25 +39,26 @@ function fetchdata(){
   .onSnapshot(function(querySnapshot) {
 
     querySnapshot.forEach(function(doc) {
-        
-        document.getElementById("listofdailyquizes").innerHTML += `
-        <a href="quiz.html?quizid=${doc.id}" style="text-decoration: none;">
-        <li class="li-list">
-        <div class="dailyquizhead">
-        ${doc.data().Name}
-        </div>
-          ${doc.data().category}<br/>
-          <a href="leaderboard.html?from=dailyquizLeaderboard&id=${doc.data().id}" style="text-decoration: none;">
-          <button class="right btn-leaderboard">Leaderboard</button>
-          </a>
-          <div class="right top">${doc.data().Date}</div>
-        </li>
-        </a>
-        `
+        if(doc.id!="tempData"){
 
-    });
-}); 
-  
+                document.getElementById("listofdailyquizes").innerHTML += `
+                <a href="quiz.html?quizid=${doc.id}" style="text-decoration: none;">
+                <li class="li-list">
+                <div class="dailyquizhead">
+                ${doc.data().Name}
+                </div>
+                  ${doc.data().category}<br/>
+                  <a href="leaderboard.html?from=dailyquizLeaderboard&id=${doc.data().id}" style="text-decoration: none;">
+                  <button class="right btn-leaderboard">Leaderboard</button>
+                  </a>
+                  <div class="right top">${doc.data().Date}</div>
+                </li>
+                </a>
+                `
+
+            });
+        }); 
+  }
 database.collection("currentaffairs")
   .onSnapshot(function(querySnapshot) {
 
